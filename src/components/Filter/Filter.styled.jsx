db4109f7c6ba55a307deb42d2cc06c3f72110dd0
Andriy-Lik <alan321987@gmail.com>
2023-02-06 00:25:29 +0200

    Ex 2 Phonebook

---
+++ src/components/Filter/Filter.styled.jsx
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+export const FilterDiv = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 400px;
+    margin-bottom: 20px;
+`;
+
+export const FilterLabel = styled.label`
+    font-size: 16px;
+    font-weight: 500;
+`;
+
+export const FilterInput = styled.input`
+    width: 200px;
+    height: 30px;
+    border: 1px solid black;
+    border-radius: 3px;
+`;
---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    max-width: 1200px;
+`;
+
+export const Title = styled.h2`
+    font-size: 30px;
+    font-weight: 500;
+    margin: 0 0 20px;
+`;
